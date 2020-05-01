e8494a1cca50b5bdfd7e7d7a328f1d5f59183593
abdelp <juniorperezpy@gmail.com>
2020-05-01 12:02:23 -0400

    Add initial attendee structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :events
+  has_many :created_events, class_name: 'Event', foreign_key: 'user_id'
+  has_many :attended_events, through: :attendee_event, foreign_key: 'attendee_id', class_name: 'Event'
 end
