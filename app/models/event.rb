47b194c15ae7787d3a1ac19efee3941e59d731b1
abdelp <juniorperezpy@gmail.com>
2020-05-01 15:57:44 -0400

    Add scope for upcomming_events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :attendee_events
   has_many :attendees, through: :attendee_events, class_name: 'User', foreign_key: 'user_id'
-  scope :upcoming_events, -> { where('date > ?', DateTime.now) }
+  scope :upcoming_events, -> { where('date >= ?', Date.today.strftime("%Y-%m-%d")) }
 end
