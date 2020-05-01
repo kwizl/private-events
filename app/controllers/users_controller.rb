4a01f19675a24842561836856cf5ed502456b414
abdelp <juniorperezpy@gmail.com>
2020-05-01 14:03:06 -0400

    Add initial method for upcoming events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @upcoming_events = @user.attended_events.upcoming_events
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
 
   has_many :created_events, class_name: 'Event', foreign_key: 'user_id'
   has_many :attendee_events
-  has_many :attended_events, through: :attendee_events, foreign_key: 'user_id', source: 'Event'
+  has_many :attended_events, through: :attendee_events, source: :event, foreign_key: 'user_id'
 end
