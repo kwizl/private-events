7e0139ba28090552869223acfbdbab27f6ef2359
abdelp <juniorperezpy@gmail.com>
2020-05-06 11:58:58 -0400

    mend

--- spec/requests/user_sessions_request_spec.rb
+++ spec/requests/user_sessions_request_spec.rb
@@ -3,20 +3,21 @@ require 'capybara/rails'
 
 RSpec.describe "UserSessions", type: :request do
   describe 'the signin process', type: :feature do
-    let (:user) { FactoryBot.build(:user) }
+    let(:user) { create(:user, name: 'jack', password: '12345') }
 
-    scenario 'login page' do
+    scenario 'sets the user name in the session' do
       visit login_url
       within 'form' do
         fill_in "Name", with: user.name
       end
       click_button 'Login'
-      expect(status_code).to eq 200
+      expect(page.get_rack_session_key('name')).to eq("jack")
+      page.instance_variable_set(:@touched, false)
     end
 
-    scenario 'logout' do
+    scenario 'removes name key from session when logout' do
       visit logout_url
-      expect(status_code).to eq 200
+      expect { page.get_rack_session_key('name') }.to raise_error(KeyError)
     end
   end
 end
