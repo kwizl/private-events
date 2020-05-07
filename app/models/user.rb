54139f4e056a7c7c0cb5aa1cb8a513f5101aab04
kwizl <karis.njoroge@gmail.com>
2020-05-08 00:56:09 +0300

    Rectify linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :created_events, class_name: 'Event', foreign_key: 'user_id', dependent: :destroy
+  has_many :created_events, class_name: 'Event', foreign_key: 'user_id', dependent: :destroy, inverse_of: :creator
   has_many :attendee_events, dependent: :destroy
   has_many :attended_events, through: :attendee_events, source: :event, foreign_key: 'user_id'
 end
