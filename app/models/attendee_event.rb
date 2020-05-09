95cb0aa10418dd65573a1af844dc0cfaa8b38019
kwizl <karis.njoroge@gmail.com>
2020-05-09 03:02:43 +0300

    Fix the attendee_event model inverse_of issue

--- app/models/attendee_event.rb
+++ app/models/attendee_event.rb
@@ -1,4 +1,4 @@
 class AttendeeEvent < ApplicationRecord
-  belongs_to :attendee, class_name: 'User', foreign_key: 'user_id', inverse_of: :attendee_event
+  belongs_to :attendee, class_name: 'User', foreign_key: 'user_id', inverse_of: :attended_events
   belongs_to :event
 end
