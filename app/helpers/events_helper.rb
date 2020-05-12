117449c8eee6b28d19014d41a8c754f77adf964d
abdelp <juniorperezpy@gmail.com>
2020-05-12 11:12:45 -0400

    Remove logic from views

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -9,4 +9,33 @@ module EventsHelper
 
     html.html_safe
   end
+
+  def events_list(events)
+    html = ''
+    events.each do |e|
+      html += <<-HTML
+      <tr scope="row">
+        <td>#{e.name}</td>
+        <td>#{e.description}</td>
+        <td>#{e.date}</td>
+        <td>#{e.creator.name}</td>
+        <td>#{link_to 'view', e}</td>
+      </tr>
+      HTML
+    end
+    html.html_safe
+  end
+
+  def attendees_list(attendees)
+    html = ''
+
+    attendees.each do |a|
+      html += <<-HTML
+      <div class="row">
+      <p>#{a.name}</p>
+      </div>
+      HTML
+    end
+    html.html_safe
+  end
 end
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,13 @@
 module UserHelper
+  def users_list(users)
+    html = ''
+    users.each do |u|
+      html += <<-HTML
+          <tr scope="row">
+          <td>#{u.name}</td>
+          </tr>
+            HTML
+    end
+    html.html_safe
+  end
 end
