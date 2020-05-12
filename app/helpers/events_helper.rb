30dad3924cabc59e0f1e982b06f15de3f1078d58
abdelp <juniorperezpy@gmail.com>
2020-05-11 20:10:19 -0400

    Fix rubocop errors

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,12 @@
 module EventsHelper
+  def assist(event_id)
+    html = ''
+    html += if !current_user.attended_events.exists?(event_id)
+              button_to 'Attend', assist_event_path(event_id: event_id), { class: 'btn btn-primary' }
+            else
+              button_to 'Attend', assist_event_path(event_id: event_id), { class: 'btn btn-secondary', disabled: true }
+            end
+
+    html.html_safe
+  end
 end
